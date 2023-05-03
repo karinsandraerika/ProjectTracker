13f1eb629601cb83dcfc3c42b14bdbe2dcf0aad1
sandra erika persson <sandra.erika.persson@gmail.com>
2023-05-03 14:51:35 +0200

    Database context

--- ProjectTracker/Data/DatabaseContext.cs
+++ ProjectTracker/Data/DatabaseContext.cs
@@ -1,11 +1,19 @@
 ﻿using System;
+using ProjectTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace ProjectTracker.Data
 {
-	public class DatabaseContext
+	public class DatabaseContext : DbContext
 	{
-		public DatabaseContext()
-		{
-		}
-	}
+        public DatabaseContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Person> Person { get; set; } = default!; //Check default!
+        public DbSet<Project> Project { get; set; } = default!;
+        public DbSet<ProjectItem> ProjectItem { get; set; } = default!;
+    }
 }
 
