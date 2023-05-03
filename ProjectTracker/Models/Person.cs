73cd036fffd3f9b36c9111362c5f84e2a7f6f57b
sandra erika persson <sandra.erika.persson@gmail.com>
2023-05-03 13:38:39 +0200

    Added props

--- ProjectTracker/Models/Person.cs
+++ ProjectTracker/Models/Person.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace ProjectTracker.Models
 {
 	public class Person
 	{
-		public Person()
-		{
-		}
-	}
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
 }
 
